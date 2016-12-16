360cfab0c6ed94ab25ce06e476569799d3b12481
Megan Davis <davisma24@vcu.edu>
2016-12-15 20:57:08 -0500

    Added tables - including their css/js, reworking to php for DB access

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+
+
+
+<!--- actual content -->
+<div id="about-banner" style = "background-color: #fff4d3;">
+	<div class="centering" style="text-align: center;">
+		<h1> The Library of Codexes currently contains: 1735 individual works, 70 authors, 3 collections, and 8 games.  </h1>
+	</div>
+</div>
+<br/>
+<div class="centering">
+	<h1><b>Our Mission</b></h1>
+	<p>Videogames have always excelled at creating complex worlds filled with creatures, items, and that one mind-numbingly slow NPC you <em>must</em> escort. These games often have a plethora of in-game text that can be difficult to collect and read in-game. Library of Codexes sole mission is to create an archive of in-game text that is easily accessible, either online or offline (ebooks).</p>
+</div>
+
+<?php include('footer.html'); ?>
---
+++ home.php
@@ -0,0 +1,5 @@
+<?php 
+	include('header.php');
+	include('home.html');
+	include('footer.html');
+?>
