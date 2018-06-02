9f12c7bde7866b92444bf497b6960f3b128e33a8
unknown <davisma24@vcu.edu>
2018-06-02 08:53:35 -0400

    Updating data_entry script

--- perl scripts/data_entry.pl
+++ perl scripts/data_entry.pl
@@ -22,9 +22,10 @@
 ##############################################################
 use warnings;
 use Data::Dumper qw(Dumper);
+use HTML::Entities;
 $Data::Dumper::Sortkeys = 1;
 use DBI;
-use Encode;
+
 
 my $filename = $ARGV[0];
 my $choice = $ARGV[1];
@@ -40,16 +41,45 @@ my $temp = 0; #temp is used to count off lines in instances where the CSV inform
 my $tempnum = 0; #used to assign codex information to the right spots
 my $tempstring; #hold the text being read in from the text file
 my %codexhash;
-my $author_count = -1;   #faster than quering the highest author ID everytime
+my $author_count = 10000;   #faster than quering the highest author ID everytime
 my $series_id = 16;
 my $game_id = 42;
 my $codex_id;
 
-witcher();
+
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";
+while(my $row = <$fh>)
+{
+	print encode_entities($row);
+}
+
+sub insert_game{
+	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";
+	while(my $row = <$fh>)
+	{
+		chomp $row;
+		$row =~ s/\\xA0/ /g;
+
+		my @words = split('","', $row);
+
+		## Title fixes
+		$words[4] =~ s/"//g;
+		$words[4] =~ s/([\w']+)/$1/g;
+			
+		text_sanitize($words[5]);
+
+		$codexhash{$tempnum}{Title} = $words[4];
+		$codexhash{$tempnum}{Author} = 10000;
+			
+		$tempnum++;
+		
+		$string .= $row;
+	}	
+}
 
 sub witcher{
 	### Part 1 - Open File ###
-	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";	
+	
 	### Part 2 - Read file and put into hash
 	while(my $row = <$fh>)
 	{
@@ -123,6 +153,8 @@ sub check_author_exists{
 	$query->finish();
 }
 
+
+## REMOVE THIS ###
 sub check_for_null{
 	for my $item (keys %codexhash){
 		for my $subitem(keys %{$codexhash{$item}}){
@@ -145,6 +177,8 @@ sub check_for_null{
 		}
 	}
 }
+
+### REMOVE ABOVE ###
 
 sub alert_null{
 	for my $item (keys %codexhash){
@@ -185,6 +219,8 @@ sub insert_author{
 	$author_count++;	
 }
 
+
+
 sub insert_codex{
 	my $query = $myConnection->prepare("INSERT INTO CODEXES (CODEX_ID, CODEX_TITLE, CODEX_TEXT, FK_AUTHOR_ID, FK_GAME_ID, FK_SERIES_ID) values (?,?,?,?,?,?)");
 
@@ -194,15 +230,17 @@ sub insert_codex{
 		$query->finish();
 		
 		
-		insert_codex_authors($codexhash{$item}{Author}, $codex_id);
+		#insert_codex_authors($codexhash{$item}{Author}, $codex_id);
 	}
 }
 
+## Ignore till properly fixed
 sub insert_codex_authors{
 	my $query2 = $myConnection->prepare("INSERT INTO CODEXES_AUTHORS (FK_AUTHOR_ID, FK_CODEX_ID, FK_GAME_ID) values (?,?,?)");
 	$query2->execute($_[0], $_[1], $game_id) or die $DBI::errstr;
 	$query2->finish();
 }
+###
 
 sub print_hash{
 	#my %tempHash = $_[0];
@@ -271,300 +309,6 @@ sub text_sanitize{
 
 	$codexhash{$tempnum}{Text} = $text;	
 }
-
-######################### Elder Scrolls Subroutines #############################
-sub elder_scrolls_check{
-	### Part 1 - Open File ###
-	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";	
-	### Part 2 - Read file and put into hash
-	while(my $row = <$fh>)
-	{
-		chomp $row;
-		$row =~ s/\\xA0/ /g;
-		$row =~ s/\\x92/'/g;
-
-		if($temp < 2)
-		{
-			$tempstring .= $row;
-			$temp++;
-		}
-		else{
-			my @words = split('","', $tempstring);
-
-			## Title fixes
-			$words[0] =~ s/"//g;
-			$words[0] =~ s/([\w']+)/\u\L$1/g;
-			## Author Fixes
-			$words[2] =~ s/A(.)*://g;   
-			$words[2] =~ s/\\xA0//g;
-			$words[2] =~ s/^\s+|\s+$//g;
-
-			#In some instances the web scrapper had two text columns
-			#Whichever is larger is used as the text (not sure on accuracy)
-			if(scalar @words == 5) {
-				if(length($words[3]) > length($words[4])) {
-					text_sanitize($words[3]);
-				}	
-				else {
-					text_sanitize($words[4]);
-				}			
-			}
-			elsif(scalar @words == 6)
-			{
-				if(length($words[3]) > length($words[4]) && length($words[3]) > length($words[5])) {
-					text_sanitize($words[3]);
-				}
-				elsif(length($words[4]) > length($words[3]) && length($words[4]) > length($words[5])){
-					text_sanitize($words[4])
-				}	
-				else {
-					text_sanitize($words[5]);
-				}		
-			}
-			else{	
-				text_sanitize($words[3]);
-			}
-
-			$codexhash{$tempnum}{Title} = $words[0];
-			$codexhash{$tempnum}{Author} = $words[2];
-			
-			$temp = 1;
-			$tempstring = "";
-			$tempstring .= $row;
-			$tempnum++;
-		}
-		$string .= $row;
-	}
-
-	### Part 3 - Check Empty (before enabling this run ascii to make sure all replaces are accounted for)
-	check_for_null();
-	### Part 4 - Check ASCII #####
-	replace_ascii();
-}
-
-sub elder_scrolls_insert{
-	### Part 1 - Open File ###
-	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";	
-	### Part 2 - Read file and put into hash
-	while(my $row = <$fh>)
-	{
-		chomp $row;
-		$row =~ s/\\xA0/ /g;
-
-		if($temp < 2)
-		{
-			$tempstring .= $row;
-			$temp++;
-		}
-		else{
-			my @words = split('","', $tempstring);
-
-			## Title fixes
-			$words[0] =~ s/"//g;
-			$words[0] =~ s/([\w']+)/\u\L$1/g;
-			## Author Fixes
-			$words[2] =~ s/A(.)*://g;   
-			$words[2] =~ s/\\xA0//g;
-			$words[2] =~ s/^\s+|\s+$//g;
-
-			#In some instances the web scrapper had two text columns
-			#Whichever is larger is used as the text (not sure on accuracy)
-			if(scalar @words == 5) {
-				if(length($words[3]) > length($words[4])) {
-					text_sanitize($words[3]);
-				}	
-				else {
-					text_sanitize($words[4]);
-				}			
-			}
-			elsif(scalar @words == 6)
-			{
-				if(length($words[3]) > length($words[4]) && length($words[3]) > length($words[5])) {
-					text_sanitize($words[3]);
-				}
-				elsif(length($words[4]) > length($words[3]) && length($words[4]) > length($words[5]))
-				{
-					text_sanitize($words[4])
-				}	
-				else {
-					text_sanitize($words[5]);
-				}		
-			}
-			else{	
-				text_sanitize($words[3]);
-			}
-
-			$codexhash{$tempnum}{Title} = $words[0];
-			$codexhash{$tempnum}{Author} = $words[2];
-			
-			$temp = 1;
-			$tempstring = "";
-			$tempstring .= $row;
-			$tempnum++;
-		}
-		$string .= $row;
-	}
-
-	### Part 3 - Check Empty (before enabling this run ascii to make sure all replaces are accounted for)
-	check_for_null();
-	### Part 4 - Check ASCII #####
-	replace_ascii();
-	### Run the second time
-	### Part 4 - Check if Author exists in MYSQL, if author exists replace 
-	authors_call();
-	### Part 5 - Add Codexes to DB
-	insert_codex();
-}
-
-######################### Dishonored Subroutines ##################################
-sub dishonored2_audiodiographs{
-	### Part 1 - Open File ###
-	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";	
-	### Part 2 - Read file and put into hash
-	while(my $row = <$fh>)
-	{
-		chomp $row;
-		my @words = split('","', $row);
-		
-		$words[3] =~ s/\\xA0//g;
-		$words[3] =~ s/\},\{/\},~\{/g;
-		$words[3] =~ s/^\s+|\s+$//g;
-
-		$codexhash{$tempnum}{Title} = $words[2];
-		$codexhash{$tempnum}{Author} = $words[4];
-
-				
-		#print "-------Text Split--------\n";
-		#print Dumper @text;
-		if($words[4] =~ m/ recorded (by|By) /)
-		{
-			@author = split(/recorded (by|By)/, $words[4]);
-			$author[2] =~ s/^\s+|\s+$//g;
-			$author[2] =~ s/\.//g;
-			$author[2] =~ s/\"//g;
-			$codexhash{$tempnum}{Author} = $author[2];
-		}	
-
-		my $tempVar = scalar @text;
-		my $string1 = $text[$tempVar-1];
-		my $thestring;
-
-		my @text = split ('~', $words[3]);
-		foreach $a (@text){
-			if($a =~ m/ can be found/)
-			{
-				
-			}	
-			else{
-				$thestring .= $a;
-			}
-		}		
-		text_sanitize($thestring);	
-
-		$tempnum++;
-	}
-	
-	
-	### Part 3 - Check Empty (before enabling this run ascii to make sure all replaces are accounted for)
-	check_for_null();
-	### Part 4 - Check ASCII #####
-	replace_ascii();	
-	### Part 4 - Check if Author exists in MYSQL, if author exists replace 
-	#authors_call();
-	### Part 5 - Add Codexes to DB
-	#insert_codex();
-	print_hash();
-}
-
-sub dishonored2_notes{
-	### Part 1 - Open File ###
-	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open";	
-	### Part 2 - Read file and put into hash
-	while(my $row = <$fh>)
-	{
-		chomp $row;
-		my @words = split('","', $row);
-		
-		$words[3] =~ s/\\xA0//g;
-		$words[3] =~ s/\},\{/\},~\{/g;
-		$words[3] =~ s/^\s+|\s+$//g;
-
-		$codexhash{$tempnum}{Title} = $words[2];
-		$codexhash{$tempnum}{Author} = "temp";
-		
-		if($words[2] =~ m/ (by|By) /)
-		{
-			@author = split(/(by|By)/, $words[2]);
-			$author[2] =~ s/^\s+|\s+$//g;
-			$codexhash{$tempnum}{Author} = $author[2];
-			$codexhash{$tempnum}{Title} = $author[0];
-		}	
-
-		#used to delete the extra text the web scrapper pulls 
-		my $tempVar = scalar @text;
-		my $string1 = $text[$tempVar-1];
-		my $thestring;
-
-		my @text = split ('~', $words[3]);
-		foreach $a (@text){
-			if($a =~ m/ can be found/) #both of these are sentences from the wiki that aren't codex text
-			{
-				
-			}
-			elsif($a =~ m/ can be looted/)
-			{
-
-			}	
-			else{
-				$thestring .= $a;
-			}
-		}		
-		text_sanitize($thestring);	
-		$tempnum++;
-	}
-	
-	### Part 3 - Check Empty (before enabling this run ascii to make sure all replaces are accounted for)
-	check_for_null();
-	### Part 4 - Check ASCII #####
-	replace_ascii();	
-	### Part 4 - Check if Author exists in MYSQL, if author exists replace 
-	authors_call();
-	### Part 5 - Add Codexes to DB
-	insert_codex();
-	print_hash();
-}
-
-
-## Needs more documentation -- was used to tell if last line had the author
-sub detect_authors{
-	my $query = $myConnection->prepare("SELECT CODEX_ID, CODEX_TEXT FROM CODEXES WHERE FK_GAME_ID = 11");
-	$query->execute();
-	while(@data = $query->fetchrow_array()){
-		my $codex_id = $data[0];
-		my @text = split('\n',$data[1]);
-		my $tempString = $text[scalar @text-1];
-		#print $tempString . ": ";
-		my @matched = split('by ',$tempString);
-		if(defined($matched[1]))
-		{
-			my @punc = split (/[,|.|?]/, $matched[1]);
-			my $statement = $myConnection->prepare("INSERT INTO AUTHORS (AUTHOR_ID, TITLE, FIRST_NAME, LAST_NAME, BIOGRAPHY, FK_GAME_ID) values (?,?,?,?,?,?)");
-			$statement->execute($author_count, "", $punc[0], "", "[To be added]", $_[0]) or die $DBI::errstr;	
-			$statement->finish();
-
-			#print $punc[0] . "\n";
-			my $update = $myConnection->prepare("UPDATE CODEXES SET FK_AUTHOR_ID = ? WHERE CODEX_ID = ?");
-			$update->execute($author_count,$codex_id) or die $DBI::errstr;
-			$update->finish();
-
-			$author_count++;
-		}
-		
-		
-	}
-}
-
-
 
 sub dragon_age{
 	### Part 1 - Open File ###
